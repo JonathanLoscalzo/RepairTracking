34c9065e6e84257e41920a183934893c7e56ddcf
mariano <mariano_martinelli@yahoo.com.ar>
2018-10-24 22:42:28 -0300

    Tooltip mejorado

--- src/RepairTracking.Web/ClientApp/src/modules/clients/presentational/components/ButtonEditarCliente.js
+++ src/RepairTracking.Web/ClientApp/src/modules/clients/presentational/components/ButtonEditarCliente.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { FaEdit } from 'react-icons/fa'
 import { UncontrolledTooltip as Tooltip } from 'reactstrap'
 
+const delay = {show: 500, hide:0};
+
 const ButtonEditarCliente = ({ goToEdit, id }) => (
     <div>
-        <span href="#" id="UncontrolledTooltipExample">
-            <button type="button" className="btn btn-primary btn-sm" onClick={() => goToEdit(id)}>
+        <span href="#" id={'miraElOtroId' + id}>
+            <button type="button" className="mr-2 mb-2 mt-2 btn btn-primary btn-sm" onClick={() => goToEdit(id)}>
                 <FaEdit />
             </button>
         </span>
-        <Tooltip placement="top" target="UncontrolledTooltipExample">
+        <Tooltip delay={delay} placement="top" target={'miraElOtroId' + id}>
             Editar Cliente
         </Tooltip>
     </div>
--- src/RepairTracking.Web/ClientApp/src/modules/clients/presentational/components/ButtonVerCliente.js
+++ src/RepairTracking.Web/ClientApp/src/modules/clients/presentational/components/ButtonVerCliente.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { FaList } from 'react-icons/fa'
 import { UncontrolledTooltip as Tooltip } from 'reactstrap'
 
+const delay = {show: 500, hide:0};
+
 const ButtonVerCliente = ({ goToShow, id }) => (
     <div>
-        <span href="#" id="UncontrolledTooltipExample2">
-            <button type="button" className="btn btn-info btn-sm" onClick={() => goToShow(id)}>
+        <span href="#" id={'miraEsteId' + id}>
+            <button type="button" className="ml-4 mb-2 mt-2 btn btn-info btn-sm" onClick={() => goToShow(id)}>
                 <FaList />
             </button>
         </span>
-        <Tooltip placement="top" target="UncontrolledTooltipExample2">
+        <Tooltip delay={ delay } placement="top" target={'miraEsteId' + id}>
             Ver Cliente
         </Tooltip>
     </div>
